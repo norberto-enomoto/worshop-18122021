8d32d2e1f6a5bf678f6cdaaeef0aae8559a5e4e8
Norberto Enomoto <norberto.enomoto@gmail.com>
2021-12-18 14:02:38 -0300

    Retirado log

--- src/main/java/com/example/workshop/springboot/service/UserService.java
+++ src/main/java/com/example/workshop/springboot/service/UserService.java
@@ -22,7 +22,6 @@ public class UserService {
 
   public Iterable<UserModel> getUsers() {
     log.info("Recuperando todos users");
-    log.info("teste do Github Action");
     return userRepository.findAll();
   }
 
